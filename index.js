2361c585e838d5b8be2f2a802ca1ebd959c7624b
Bert Kleewein <bertk@microsoft.com>
2016-09-28 21:30:47 -0700

    checkpoint

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var isHostUp = require('is-host-up');
+var arp = require('node-arp');
 
 var hostListFileName = '/home/bert/mac.json';
 
@@ -6,14 +8,29 @@ var getHostList = function(done) {
   fs.readFile(hostListFileName, 'utf8', function (err, data) {
     if (err) return done(err);
     var obj = JSON.parse(data);
-    done(null,obj);
+    var hostList = {};
+    for (var i = 0, len = obj.length; i < len; i++) {
+      hostList[obj[i].macaddress] = obj[i];
+    }
+    done(null,hostList);
   });
 };
 
-getHostList(function(err,data) {
+var hostList;
+getHostList(function(err,hostList) {
   if (err) {
     console.log('err:' + err);
   } else {
-    console.log(JSON.stringify(data));
+    Object.keys(hostList).forEach(function(key) {
+      var x = hostList[key];
+      isHostUp(x.lastip, function(err, isUp) {
+        console.log(x.lastip);
+        console.log(err);
+        console.log(isUp);
+        if (isUp) {
+          console.log(x.compdesc + ' is up');
+        }
+      });
+    });
   }
 });
